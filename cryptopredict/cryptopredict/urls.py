57bb53ac5391e698d83f859d387dac817a8a9e2f
karnnan <rishankvmadayi@gmail.com>
2024-08-09 14:47:16 +0530

    news2

--- cryptopredict/cryptopredict/urls.py
+++ cryptopredict/cryptopredict/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users.urls')),
     path('chart/',include('charts.urls')),
+    path('new/',include('news.urls'))
 ]
 urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- cryptopredict/news/views.py
+++ cryptopredict/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def newz(request):
+    api_request=request.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api=json.loads(api_request.content)
+    return render(request,'newz.html,{'api':api}')
