5253c17a8bf23052de955ec861e6dea04397b947
karnnan <rishankvmadayi@gmail.com>
2024-08-14 11:40:33 +0530

    newstart

--- cryptopredict/news/views.py
+++ cryptopredict/news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+import requests
+import json
 # Create your views here.
 def newz(request):
-    api_request=request.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api=json.loads(api_request.content)
-    return render(request,'newz.html,{'api':api}')
+    return render(request,'newz.html',{'api':api})
