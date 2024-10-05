4c3d87a4d69d5b03155ab53b29230247102cd374
rishankv <rishankvmadayi@gmail.com>
2024-10-05 14:33:49 +0530

    newsfinished

--- cryptopredict/news/views.py
+++ cryptopredict/news/views.py
@@ -3,6 +3,8 @@ import requests
 import json
 # Create your views here.
 def newz(request):
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,SOL,XRP,POL,LTC,SHIB,ADA,DOG,TRX&tsyms=USD")
+    price = json.loads(price_request.content)
     api_request=requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api=json.loads(api_request.content)
-    return render(request,'newz.html',{'api':api})
+    return render(request,'newz.html',{'api':api , 'price':price})
